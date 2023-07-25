669af4bd64f5ebb87de380a3c53a22c3894039bc
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-25 13:45:05 +0300

    Add files via upload

--- fsModule.js
+++ fsModule.js
@@ -1,5 +1,10 @@
 
 
+
+// fs callback style
+
+/*
+
 const fs = require('fs');
 const dataInfo = 'Some Data Information';
 
@@ -8,13 +13,29 @@ fs.writeFile('./first.txt', dataInfo, (err) => {
         console.log(err);
     } else {
         console.log('File was written');
+
+        fs.appendFile('./first.txt', '\n One more line', (err) => {
+            if(err){
+                console.log(err);
+            } else {
+                console.log('Appended file');
+            }
+        });
     }
 }); 
 
-fs.appendFile('./first.txt', '\n One more line', (err) => {
-    if(err){
-        console.log(err);
-    } else {
-        console.log('Appended file');
-    }
+*/
+
+// fs Promise style
+
+const fs = require('fs/promises');
+
+const info = 'Some Server Information';
+
+fs.writeFile('./second.txt', info)
+.then(() => {
+    console.log('File named "second.txt" was written');
+})
+.catch((err) => {
+    console.log(err);
 });
