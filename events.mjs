b68eb9e672984b3256acab78a73be8807d2cc573
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-27 14:33:40 +0300

    Add files via upload

--- events.mjs
+++ events.mjs
@@ -6,16 +6,32 @@ import {EventEmitter } from 'events'
 
 const myEmitter = new EventEmitter();
 
-myEmitter.on('timeout', () => {
-    console.log('Timeout event');
+myEmitter.on('timeout', (secondsQuantity) => {
+    console.log(`Timeout event in ${secondsQuantity} seconds`);
 });
 
 //myEmitter.emit('timeout');
 
 setTimeout(() => {
-    myEmitter.emit('timeout');
+    myEmitter.emit('timeout', 2);
 }, 2000);
 
 setTimeout(() => {
-    myEmitter.emit('timeout');
+    myEmitter.emit('timeout', 4);
 }, 4000);
+
+myEmitter.once('singleEvent', () => {
+    console.log('Single Event');
+});
+
+
+
+setTimeout(() => {
+    myEmitter.emit('singleEvent');
+}, 2000);
+
+setTimeout(() => {
+    myEmitter.emit('singleEvent');
+}, 4000);
+
+
