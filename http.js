d631446c112e06757b9091177758bb26b427ee69
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-30 16:42:21 +0300

    Add files via upload
    several http ways

--- http.js
+++ http.js
@@ -1,11 +1,42 @@
+
+
 const http = require('http');
 const PORT = 5000;
 
+const comments = [
+    {id: 100, text: "First Comment"},
+    {id: 101, text: "Second Comment"},
+    {id: 102, text: "Third Comment"}
+];
+
+
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Greetings');
+    if(req.url === '/http'){
+        console.log(req);
+        res.statusCode = 200;
+        res.setHeader = ('Content-Type', 'text/plain');
+        res.write('<html><body><div>');
+        res.write('Hello');
+        res.write('</div></body></html>');
+        return res.end('Greetings');
+    }
+
+    if(req.url === '/text'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plan');
+        return res.end('Some text');
+    }
+
+    if(req.url === '/json'){
+        res.statusCode = 200;
+        res.setHeader = ('Content-Type', 'application/json');
+        return res.end(JSON.stringify(comments));
+    }
 });
 
 server.listen(PORT, () => {
     console.log(`Server was launched on port ${PORT}`);
 });
+
+
+
