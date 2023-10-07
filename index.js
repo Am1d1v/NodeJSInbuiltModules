e53c3f4f4236c5e38166be28b941ea9c355745c2
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-07 19:04:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
+const fs = require('fs');
 
+const WriteStream = fs.createWriteStream('./file.txt');
 
-const http = require('http');
-const PORT = 5000;
-const {getHTML, getText, getComments, handleNotFound, postComment} = require('./handlers');
+WriteStream.write('Some Data');
+WriteStream.write('\n');
+WriteStream.write('Info');
 
-
-const server = http.createServer((req, res) => {
-    if(req.method = 'GET' && req.url === '/http'){
-       return getHTML(req, res);
-    }
-
-    if(req.method = 'GET' && req.url === '/text'){
-        return getText(req, res);
-    }
-
-    if(req.method = 'GET' && req.url === '/comments'){
-        return getComments(req, res);
-    }
-
-    if(req.method = 'POST' && req.url === '/comments'){
-        return postComment(req, res);
-    }
-
-    handleNotFound(req, res);
-});
-
-server.listen(PORT, () => {
-    console.log(`Server was launched on port ${PORT}`);
-});
-
-
+WriteStream.end();
 
